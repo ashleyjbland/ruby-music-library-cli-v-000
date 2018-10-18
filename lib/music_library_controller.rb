594c81687a5c690ea55e0c86ba938343d28113f7
Ashley Bland <ashleyjbland@gmail.com>
2018-10-18 21:43:22 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -76,10 +76,7 @@ class MusicLibraryController
     intro = puts "Which song number would you like to play?"
     input = gets.chomp
     if input.to_i-1 > 0 && input.to_i-1 <= list_songs.count
-      binding.pry
       list_songs[input.to_i-1]
-    else
-      intro
     end
   end
 
