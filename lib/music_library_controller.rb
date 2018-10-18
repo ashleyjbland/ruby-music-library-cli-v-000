898f4d5705f816d22293499bb1648664dd4c6b0b
Ashley Bland <ashleyjbland@gmail.com>
2018-10-18 22:01:40 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,6 +25,12 @@ class MusicLibraryController
         list_songs
       elsif input == "list artists"
         list_artists
+      elsif input == "list genres"
+        list_genres
+      elsif input == "list artist"
+        list_songs_by_artist
+      elsif input == "list genre"
+        list_songs_by_genre
       end
     end
   end
